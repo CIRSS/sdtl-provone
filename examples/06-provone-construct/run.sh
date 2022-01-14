a740de2b54a6d69fd255b45ffb50b556f741a336
Timothy McPhillips <tmcphillips@absoluteflow.com>
2022-01-14 15:58:34 -0800

    Formatting

--- examples/06-provone-construct/run.sh
+++ examples/06-provone-construct/run.sh
@@ -101,33 +101,33 @@ geist report << '__END_REPORT_TEMPLATE__'
     {{{
         {{ include "../common/sdth.g" }}
     }}}
-                                                                                                                            \\
-    {{ range $VarProducer := (select_variable_producers | rows) }}                                                     \\
-        {{ with $StepName := (index $VarProducer 0) }}                                                                 \\                   
-        {{ with $VarId := (index $VarProducer 1) }}                                                               \\
-        {{ with $VarName := (index $VarProducer 2) }}                                                             \\
-        {{ with $PortId := (var_out_port_id $StepName $VarName) }}                            \\
+                                                                        \\
+    {{ range $VarProducer := (select_variable_producers | rows) }}      \\
+        {{ with $StepName := (index $VarProducer 0) }}                  \\                   
+        {{ with $VarId := (index $VarProducer 1) }}                     \\
+        {{ with $VarName := (index $VarProducer 2) }}                   \\
+        {{ with $PortId := (var_out_port_id $StepName $VarName) }}      \\
             <{{ $StepName }}> provone:hasOutputPort <{{ $PortId }}> .  
             <{{ $PortId }}> sdth:hasVariable <{{ $VarId }}> .
-        {{ end }}                                                                                                           \\
-        {{ end }}                                                                                                           \\
-        {{ end }}                                                                                                           \\
-        {{ end }}                                                                                                           \\
-    {{ end }}                                                                                                               \\
+        {{ end }}                                                       \\
+        {{ end }}                                                       \\
+        {{ end }}                                                       \\
+        {{ end }}                                                       \\
+    {{ end }}                                                           \\
 
-    {{ range $VarConsumer := (select_variable_consumers | rows) }}                                                     \\
-        {{ with $StepName := (index $VarConsumer 0) }}                                                                 \\
-        {{ with $VarId := (index $VarConsumer 1) }}                                                               \\
-        {{ with $VarName := (index $VarConsumer 2) }}                                                             \\
-        {{ with $PortId := (var_in_port_id $StepName $VarName) }}                             \\
+    {{ range $VarConsumer := (select_variable_consumers | rows) }}      \\
+        {{ with $StepName := (index $VarConsumer 0) }}                  \\
+        {{ with $VarId := (index $VarConsumer 1) }}                     \\
+        {{ with $VarName := (index $VarConsumer 2) }}                   \\
+        {{ with $PortId := (var_in_port_id $StepName $VarName) }}       \\
             <{{ $StepName }}> provone:hasInputPort <{{ $PortId }}> .  
             <{{ $PortId }}> sdth:hasVariable <{{ $VarId }}> .
-        {{ end }}                                                                                                           \\
-        {{ end }}                                                                                                           \\
-        {{ end }}                                                                                                           \\
-        {{ end }}                                                                                                           \\
-    {{ end }}                                                                                                               \\
-                                                                                                                            \\
+        {{ end }}                                                       \\
+        {{ end }}                                                       \\
+        {{ end }}                                                       \\
+        {{ end }}                                                       \\
+    {{ end }}                                                           \\
+                                                                        \\
 __END_REPORT_TEMPLATE__
 ) | sort
 
