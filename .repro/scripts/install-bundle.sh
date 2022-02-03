140c3d280184b5745f1a4f1075c0970624d24683
Timothy McPhillips <tmcphillips@absoluteflow.com>
2022-02-02 18:35:55 -0800

    Use new install-bundle script to include geist and blaze binaries in image

---
+++ .repro/scripts/install-bundle.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# save command line arguments
+name=$1
+version=$2
+url=`eval echo ${BUNDLES_URL_TEMPLATE}`
+
+bundle_dir=${BUNDLES_DIR}/${name}-${version}
+binary=${bundle_dir}/${name}
+
+echo '***** Installing ${name} bundle *****'
+mkdir -p ${bundle_dir}
+wget -O ${binary} ${url}
+chmod u+x ${binary}
+echo -n "${bundle_dir}:" >> ~/.bundle_path
