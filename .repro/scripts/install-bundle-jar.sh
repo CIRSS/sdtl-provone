2cabacd92abd020943a1d862bc23dd0d4890eedb
Timothy McPhillips <tmcphillips@absoluteflow.com>
2022-02-02 18:58:19 -0800

    Install blazegraph jar as a bundle

--- .repro/scripts/install-bundle-jar.sh
+++ .repro/scripts/install-bundle-jar.sh
@@ -6,9 +6,7 @@ version=$2
 url=`eval echo ${BUNDLES_URL_TEMPLATE}`
 
 bundle_dir=${BUNDLES_DIR}/${name}-${version}
-binary=${bundle_dir}/${name}
+jar=${bundle_dir}/${name}.jar
 
 mkdir -p ${bundle_dir}
-wget -O ${binary} ${url}
-chmod u+x ${binary}
-echo -n "${bundle_dir}:" >> ~/.bundle_path
+wget -O ${jar} ${url}
