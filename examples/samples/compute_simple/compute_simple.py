5ea0fe1d5134d34cba97dd19c51c3417290af5f7
Timothy McPhillips <tmcphillips@absoluteflow.com>
2022-02-14 17:34:45 -0800

    Latest sample sdth example

---
+++ examples/samples/compute_simple/compute_simple.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv('df.csv')
+
+df['A'] = 3
+
+df['B'] = df.B + 6.5
+
+df['C'] = df.A - df.B
+
+temps = pd.read_csv('temps.csv')
+
+temps = temps.assign(Celsius=((temps.Fahrenheit - 32) * 5 / 9))
+
+temps = temps.assign(Kelvin=(temps.Celsius + 273))
+
+df.to_csv('outputs/df_updated.csv', index=False)
+
+temps.to_csv('outputs/temps_updated.csv', index=False)
